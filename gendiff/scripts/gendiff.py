5d2520774f60ac0f2a6989d3d64af4de558b5330
yuriy kormin <yuriy.kormin@gmail.com>
2022-03-22 14:50:28 +0300

    1 step show help by argparse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import argparse
 
 
 def main():
-    print("hello")
-
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str, help='')
+    parser.add_argument('second_file', type=str, help='')
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
