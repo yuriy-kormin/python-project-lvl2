d5bafa8f14139e05e6cbd4e3df6c116172c9848f
yuriy kormin <yuriy.kormin@gmail.com>
2022-03-22 14:55:46 +0300

    2 step add --format option to help

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,11 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
+    parser.add_argument(
+        '--format',
+        type=str,
+        help='set format of output'
+    )
     args = parser.parse_args()
 
 if __name__ == '__main__':
