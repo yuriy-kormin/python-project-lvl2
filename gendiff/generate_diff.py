dedec4b613489c87d107dd513853533b792551f8
yuriy kormin <yuriy.kormin@gmail.com>
2022-03-25 00:29:01 +0300

    codeclimate fixing

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,10 +3,9 @@ import json
 
 def generate_diff(file_path1, file_path2):
     answer = "{\n"
-    with open(file_path1) as first_file:
-        with open(file_path2) as second_file:
-            data_first_file = json.load(first_file)
-            data_second_file = json.load(second_file)
+    with open(file_path1) as first_file, open(file_path2) as second_file:
+        data_first_file = json.load(first_file)
+        data_second_file = json.load(second_file)
     keys = sorted(data_first_file | data_second_file)
     for key in keys:
         if key in data_first_file:
