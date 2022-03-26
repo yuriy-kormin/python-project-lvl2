850e57a225005c7c8d5534c147f99817f1568684
yuriy kormin <yuriy.kormin@gmail.com>
2022-03-26 15:47:02 +0300

    test-coverage workflow

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,15 +1,25 @@
 import pytest
-import os
 from gendiff import generate_diff
+from gendiff.scripts.gendiff import main
+from unittest.mock import patch
 
 right_result_path = 'tests/fixtures/output.txt'
+output_help_path = 'tests/fixtures/help_output.txt'
+
+
 @pytest.fixture
 def right_result():
     with open(right_result_path) as right_result_file:
         result_lines = right_result_file.readlines()
     return "".join(result_lines)
 
+def test_help(capsys):
+    with open(output_help_path) as right_result_file:
+        result_lines = right_result_file.readlines()
+    right_result = ''.join(result_lines)
+    assert eval('gendiff -h') == right_result
 
 def test_step4(right_result):
-    result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    result = generate_diff('tests/fixtures/file1.json',
+                           'tests/fixtures/file2.json')
     assert right_result == result
