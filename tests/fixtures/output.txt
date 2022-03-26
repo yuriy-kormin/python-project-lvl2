cc66dad62dbd7e447dfbef9c305729f659e7c511
yuriy kormin <yuriy.kormin@gmail.com>
2022-03-26 10:52:28 +0300

    make test add and add workflow

---
+++ tests/fixtures/output.txt
@@ -0,0 +1,8 @@
+{
+  - follow: False
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: True
+}
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,9 +1,15 @@
 import pytest
+import os
 from gendiff import generate_diff
-from tests.fixtures.output import right_result
 
-# @pytest.mark.usefixtures
+right_result_path = 'tests/fixtures/output.txt'
+@pytest.fixture
+def right_result():
+    with open(right_result_path) as right_result_file:
+        result_lines = right_result_file.readlines()
+    return "".join(result_lines)
+
+
 def test_step4(right_result):
-    # print(right_result())
-    result = generate_diff('tests/fixtures/file1.json','tests/fixtures/file2.json')
+    result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
     assert right_result == result
