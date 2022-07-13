fc4d916444c16ba55d8c8bdba809c4b1a8a652d5
tork <yuriy.kormin@gmail.com>
2022-07-13 23:47:09 +0400

    add option '-f' to cli input

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument(
-        '--format',
+        '-f', '--format',
         type=str,
         help='set format of output_stylish',
         choices=FORMATS.keys(),
