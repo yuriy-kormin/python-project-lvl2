2d6ecb7f88a4497a24fc07fdaf53f0d8a2bf6f4c
tork <yuriy.kormin@gmail.com>
2022-07-13 22:51:51 +0400

    default stylish formatter set in generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,7 +3,7 @@ from gendiff.file_parser import read_file
 from gendiff.formatters.formatters import FORMATS
 
 
-def generate_diff(file_path1, file_path2, format_name='stylish'):
+def generate_diff(file_path1, file_path2, format_name='plain'):
     data1 = make_inner_format(read_file(file_path1))
     data2 = make_inner_format(read_file(file_path2))
     diff = find_diff(data1, data2)
