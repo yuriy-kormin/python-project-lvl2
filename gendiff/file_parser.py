efc8c1a69cdee0654671320382beffb2d1f4566a
tork <yuriy.kormin@gmail.com>
2022-07-18 14:49:18 +0400

    separate read_file to several functions

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -3,12 +3,19 @@ import yaml
 import os
 
 
-def read_file(file_path):
-    type_file = os.path.splitext(file_path)[1]
-    if type_file == '.yaml' or type_file == '.yml':
+def get_file_format(file_path):
+    return os.path.splitext(file_path)[1]
+
+
+def parse(data, format_):
+    if format_ == '.yaml' or format_ == '.yml':
         parse_module = yaml.safe_load
-    elif type_file == '.json':
+    elif format_ == '.json':
         parse_module = json.load
+    return parse_module(data)
+
+
+def get_data(file_path):
+    format_ = get_file_format(file_path)
     with open(file_path) as file_data:
-        result = parse_module(file_data)
-    return result
+        return parse(file_data, format_)
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,10 @@
 from gendiff.inner_format import make_inner_format, find_diff
-from gendiff.file_parser import read_file
+from gendiff.file_parser import get_data
 from gendiff.formatters.formatters import FORMATS
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    data1 = make_inner_format(read_file(file_path1))
-    data2 = make_inner_format(read_file(file_path2))
+    data1 = make_inner_format(get_data(file_path1))
+    data2 = make_inner_format(get_data(file_path2))
     diff = find_diff(data1, data2)
     return FORMATS[format_name](diff)
