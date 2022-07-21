8baba61995847af53d675505dca8c9011d9de099
tork <yuriy.kormin@gmail.com>
2022-07-21 22:38:34 +0400

    stylish flat with empty files working

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,60 +1,46 @@
-# from gendiff.inner_format import get_status, get_name, get_children, \
-#     is_record, is_dir, get_value, get_old_record
 from gendiff.statuses import statuses
+from gendiff.tree import get_node_name,get_node_type,get_node_children,get_node_value
+
 
 indents = {
     statuses['+']: '  + ',
     statuses['-']: '  - ',
     statuses['=']: '    ',
-    '': '    '
 }
 
 
-def make_string(data, level, first=False, last=False):
-    if first and is_dir(data):
-        return get_indent(data, level) + get_name(data) + ': {'
-    if last:
-        return get_indent(data, level, last) + '}'
-    value = normalize_output(get_value(data))
-    # value = " "+str(value) if value else value
-    name = get_name(data)
-    return get_indent(data, level) + name + ': ' + value
-
-
-def get_indent(property, level, last=False):
-    if last:
-        return level * indents['']
-    cur_status = get_status(property)
-    cur_indent = level * indents['']
-    if cur_status in indents:
-        return cur_indent + indents[cur_status]
-    # this block executes, if value was updated
-    old_record = [get_old_record(property)]
-    result = make_block(old_record, level - 1)
-    result.append(cur_indent + indents[statuses['+']])
+def make_format(data):
+    children = get_node_children(data)
+    result = []
+    result.extend(make_block(children))
     return "\n".join(result)
 
 
-def make_block(data, level):
-    children = get_children(data, sorted_=True)
-    level += 1
-    result = []
-    if len(children) > 0:
-        for child in children:
-            if is_record(child):
-                result.append(make_string(child, level))
-            else:
-                result.append(make_string(child, level, first=True))
-                result.extend(make_block(child, level))
-                result.append(make_string(data, level + 1, last=True))
+def make_block(childrens, level=0):
+    result = ['{']
+    for child in childrens:
+        result.append(make_string(child, level))
+    result.append('}')
     return result
 
 
-def make_format(data):
-    result = ['{']
-    result.extend(make_block(data, level=-1))
-    result.append('}')
-    return "\n".join(result)
+def make_string(child, level=0):
+    status = get_node_type(child)
+    answer = ''
+    if status in indents.keys():
+        answer += level * indents[statuses['=']]
+        answer += indents[status]
+        answer += get_node_name(child) + ': '
+        answer += normalize_output(get_node_value(child))
+    elif status == statuses['!=']:
+        child_copy = child.copy()
+        child_copy['type'] = statuses['-']
+        child_copy['value'] = child_copy['old']
+        answer += make_string(child_copy, level) + '\n'
+        child_copy['type'] = statuses['+']
+        child_copy['value'] = child['value']
+        answer += make_string(child_copy, level)
+    return answer
 
 
 def normalize_output(data):
