22ce9e6fca6776e49dc068ba430f9f8a508ead58
tork <yuriy.kormin@gmail.com>
2022-07-25 13:17:09 +0400

    hexlet test flake8

--- tests/yaml_in_tests/test_empty_inputs_yml.py
+++ tests/yaml_in_tests/test_empty_inputs_yml.py
@@ -7,7 +7,7 @@ from gendiff import generate_diff
         {"file_type": 'empty', "formatter": 'stylish', 'reverse': True},
         {"file_type": 'empty', "formatter": 'plain'},
         {"file_type": 'empty', "formatter": 'plain', 'reverse': True}
-        ), indirect=True)
+), indirect=True)
 def test_empty_ymls(get_result):
     assert get_result['result'] == generate_diff(get_result['paths'][0],
                                                  get_result['paths'][1],
