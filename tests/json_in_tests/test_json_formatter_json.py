cedd0d2c391da3cd5ba4eebf73caa902ccad86ef
tork <yuriy.kormin@gmail.com>
2022-07-25 13:03:31 +0400

    flake8

--- tests/json_in_tests/test_json_formatter_json.py
+++ tests/json_in_tests/test_json_formatter_json.py
@@ -1,11 +1,10 @@
 import pytest
 from gendiff import generate_diff
 import json
-import os
 
 
-@pytest.mark.parametrize('get_json_inputs', ('empty', 'flat', 'nested')
-    , indirect=True)
+@pytest.mark.parametrize(
+    'get_json_inputs', ('empty', 'flat', 'nested'), indirect=True)
 def test_one(get_json_inputs):
     diff = generate_diff(get_json_inputs[0], get_json_inputs[1], 'json')
     assert isinstance(json.loads(diff), dict)
--- tests/yaml_in_tests/test_json_formatter_yaml.py
+++ tests/yaml_in_tests/test_json_formatter_yaml.py
@@ -1,11 +1,10 @@
 import pytest
 from gendiff import generate_diff
 import json
-import os
 
 
-@pytest.mark.parametrize('get_yaml_inputs', ('empty', 'flat', 'nested')
-    , indirect=True)
+@pytest.mark.parametrize(
+    'get_yaml_inputs', ('empty', 'flat', 'nested'), indirect=True)
 def test_one(get_yaml_inputs):
     diff = generate_diff(get_yaml_inputs[0], get_yaml_inputs[1], 'json')
     assert isinstance(json.loads(diff), dict)
