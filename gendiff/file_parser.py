4837c2f35d88957023e8a841448f6719bd911632
tork <yuriy.kormin@gmail.com>
2022-08-11 11:04:35 +0400

    refactor: add test for ValueError exception

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -2,8 +2,6 @@ import json
 import os
 import yaml
 
-from pyparsing import ParseException
-
 
 def get_file_format(file_path):
     return os.path.splitext(file_path)[1][1:]
@@ -14,7 +12,7 @@ def parse(data, file_format):
         return yaml.safe_load(data)
     elif file_format == 'json':
         return json.load(data)
-    raise ParseException('Unsupported file format')
+    raise ValueError('Unsupported file format')
 
 
 def get_data(file_path):
--- tests/unsupported_in/test_unsupported_formats.py
+++ tests/unsupported_in/test_unsupported_formats.py
@@ -2,6 +2,8 @@ from gendiff import generate_diff
 import pytest
 
 
-@pytest.mark.xfail()
+# @pytest.mark.xfail()
 def test_unsupported_file_format(input_paths):
-    assert generate_diff(input_paths[0], input_paths[1]) == 0
+    with pytest.raises(Exception) as e:
+        generate_diff(input_paths[0], input_paths[1])
+    assert str(e.value) == 'Unsupported file format'
