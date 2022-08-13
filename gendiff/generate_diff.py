4effcfc5cf8648c9d4ae573674c97e1c9cdaf53b
tork <yuriy.kormin@gmail.com>
2022-08-13 20:50:22 +0400

    refactor 3: 2 rename file_parser move functions to generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,4 +1,6 @@
-from gendiff.file_parser import get_data
+import os
+
+from gendiff.parser import parse
 from gendiff.tree import build
 from gendiff.formatter import formatting
 
@@ -6,3 +8,13 @@ from gendiff.formatter import formatting
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     diff = build(get_data(file_path1), get_data(file_path2))
     return formatting(format_name)(diff)
+
+
+def get_file_format(file_path):
+    return os.path.splitext(file_path)[1][1:]
+
+
+def get_data(file_path):
+    file_format = get_file_format(file_path)
+    with open(file_path) as file_data:
+        return parse(file_data, file_format)
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,10 +1,5 @@
 import json
-import os
 import yaml
-
-
-def get_file_format(file_path):
-    return os.path.splitext(file_path)[1][1:]
 
 
 def parse(data, file_format):
@@ -13,9 +8,3 @@ def parse(data, file_format):
     elif file_format == 'json':
         return json.load(data)
     raise ValueError(f'Unsupported file format: {file_format}')
-
-
-def get_data(file_path):
-    file_format = get_file_format(file_path)
-    with open(file_path) as file_data:
-        return parse(file_data, file_format)
