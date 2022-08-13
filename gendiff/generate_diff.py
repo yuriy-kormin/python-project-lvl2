b1525cf25c3cddfc7361c628c446dae200f994d6
tork <yuriy.kormin@gmail.com>
2022-08-13 20:52:59 +0400

    refactor 3: 3 reformat generate diff - move calling inner defs to variables

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,7 +6,9 @@ from gendiff.formatter import formatting
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    diff = build(get_data(file_path1), get_data(file_path2))
+    data1 = get_data(file_path1)
+    data2 = get_data(file_path2)
+    diff = build(data1, data2)
     return formatting(format_name)(diff)
 
 
