08b156aee2977578aa8d25506d6135ba10c4338b
tork <yuriy.kormin@gmail.com>
2022-08-15 18:29:51 +0400

    refactor 3: 4,5 reformat stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -2,16 +2,9 @@ def render_stylish(tree):
     return _node_processing(tree)
 
 
-def _build_indent(level, status=None):
-    result = level * '    '
-    if status:
-        if status == 'added':
-            return result + '  + '
-        if status == 'removed':
-            return result + '  - '
-        if status == 'equals':
-            return result + '    '
-    return result
+def _build_indent(level):
+    indent = ' ' * 4
+    return level * indent
 
 
 def _node_processing(node, level=0):
@@ -21,28 +14,39 @@ def _node_processing(node, level=0):
     formatted_value = _process_value(node.get('value'), level)
     formatted_value1 = _process_value(node.get('old_value'), level)
     formatted_value2 = _process_value(node.get('new_value'), level)
-    indent = _build_indent(level, node_type)
+    indent = _build_indent(level)
+
     if node_type == 'root':
         lines = map(lambda child: _node_processing(child, level), children)
         result = '\n'.join(lines)
         return f'{{\n{result}\n}}'
-    elif node_type == 'nested':
+
+    if node_type == 'nested':
         lines = map(lambda child: _node_processing(child, level + 1), children)
         result = '\n'.join(lines)
         return f"{indent}    {name}: {{\n{result}\n{indent}    }}"
-    elif node_type == 'updated':
-        result = [
-            _build_indent(level, 'removed') + f'{name}: {formatted_value1}',
-            _build_indent(level, 'added') + f'{name}: {formatted_value2}']
+
+    if node_type == 'updated':
+        result = [f'{indent}  - {name}: {formatted_value1}',
+                  f'{indent}  + {name}: {formatted_value2}']
         return '\n'.join(result)
-    # if node_type in ('added', 'removed', 'equals'):
-    return _build_indent(level, node_type) + name + ': ' + formatted_value
+
+    if node_type == 'added':
+        return f'{indent}  + {name}: {formatted_value}'
+
+    if node_type == 'removed':
+        return f'{indent}  - {name}: {formatted_value}'
+
+    if node_type == 'equals':
+        return f'{indent}    {name}: {formatted_value}'
+
+    raise ValueError(f'Unsupported node type: {node_type}')
 
 
 def _process_value(data, level):
     if isinstance(data, dict):
         result = ''
-        indent = _build_indent(level, 'equals')
+        indent = _build_indent(level + 1)
         for record in data:
             value = _process_value(data[record], level + 1)
             result += f'\n{indent}    {record}: {value}'
