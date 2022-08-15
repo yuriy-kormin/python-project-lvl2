70b5cf761f7ca05e9832809c8d35465fb49683da
tork <yuriy.kormin@gmail.com>
2022-08-15 19:56:49 +0400

    refactor 3: 8 reformat plain out to add dot in node_processing only for nested keys

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -3,28 +3,30 @@ def render_plain(tree):
 
 
 def _node_processing(node, path=''):
-    name = node.get('key') if not path else f'{path}.' + node.get('key')
+    property_path = f"{path}{node.get('key')}"
     children = node.get('children')
     node_type = node.get('type')
     formatted_value = _to_str(node.get('value'))
     formatted_value1 = _to_str(node.get('old_value'))
     formatted_value2 = _to_str(node.get('new_value'))
-    result = f"Property '{name}' was {node_type}"
+    result = f"Property '{property_path}' was {node_type}"
     if node_type == 'root':
         lines = filter(None, map(
             lambda child: _node_processing(child), children))
         return '\n'.join(lines)
     elif node_type == 'nested':
         lines = filter(None, map(
-            lambda child: _node_processing(child, name), children))
+            lambda child: _node_processing(child, f"{property_path}."), children))
         return '\n'.join(lines)
     elif node_type == 'added':
         return f"{result} with value: {formatted_value}"
     elif node_type == 'removed':
         return result
     elif node_type == 'updated':
+        print (formatted_value1)
         return f"{result}. From {formatted_value1} to {formatted_value2}"
-    raise ValueError(f'Unsupported node type: {node_type}')
+    elif node_type != 'equals':
+        raise ValueError(f'Unsupported node type: {node_type}')
 
 
 def _to_str(value):
@@ -36,4 +38,4 @@ def _to_str(value):
         return 'null'
     elif isinstance(value, int):
         return str(value)
-    return '\'' + str(value) + '\''
+    return f"'{str(value)}'"
