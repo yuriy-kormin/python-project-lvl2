0d36037970dc44b138cee23e61d88035e0ab9b75
tork <yuriy.kormin@gmail.com>
2022-08-15 18:44:54 +0400

    refactor 3: 6 reformat stylish - remove _process_value def

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -11,53 +11,41 @@ def _node_processing(node, level=0):
     name = node.get('key')
     children = node.get('children')
     node_type = node.get('type')
-    formatted_value = _process_value(node.get('value'), level)
-    formatted_value1 = _process_value(node.get('old_value'), level)
-    formatted_value2 = _process_value(node.get('new_value'), level)
+    formatted_value = _to_str(node.get('value'), level)
+    formatted_value1 = _to_str(node.get('old_value'), level)
+    formatted_value2 = _to_str(node.get('new_value'), level)
     indent = _build_indent(level)
-
     if node_type == 'root':
         lines = map(lambda child: _node_processing(child, level), children)
         result = '\n'.join(lines)
         return f'{{\n{result}\n}}'
-
     if node_type == 'nested':
         lines = map(lambda child: _node_processing(child, level + 1), children)
         result = '\n'.join(lines)
         return f"{indent}    {name}: {{\n{result}\n{indent}    }}"
-
     if node_type == 'updated':
         result = [f'{indent}  - {name}: {formatted_value1}',
                   f'{indent}  + {name}: {formatted_value2}']
         return '\n'.join(result)
-
     if node_type == 'added':
         return f'{indent}  + {name}: {formatted_value}'
-
     if node_type == 'removed':
         return f'{indent}  - {name}: {formatted_value}'
-
     if node_type == 'equals':
         return f'{indent}    {name}: {formatted_value}'
-
     raise ValueError(f'Unsupported node type: {node_type}')
 
 
-def _process_value(data, level):
-    if isinstance(data, dict):
-        result = ''
-        indent = _build_indent(level + 1)
-        for record in data:
-            value = _process_value(data[record], level + 1)
-            result += f'\n{indent}    {record}: {value}'
-        return f'{{{result}\n{indent}}}'
-    else:
-        return _to_str(data)
-
-
-def _to_str(data):
+def _to_str(data, level):
     if isinstance(data, bool):
         return str(data).lower()
     elif data is None:
         return 'null'
+    elif isinstance(data, dict):
+        result = ''
+        indent = _build_indent(level + 1)
+        for record in data:
+            value = _to_str(data[record], level + 1)
+            result += f'\n{indent}    {record}: {value}'
+        return f'{{{result}\n{indent}}}'
     return str(data)
